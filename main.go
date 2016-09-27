3b1cb250cb0e4c7b44d2d82c78a40a599ba4e7c3
Henryk Helsky <traone@gmail.com>
2016-09-27 17:40:30 +0200

    init

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+import "time"
+
+//import "net/http"
+
+type Dude struct {
+	year  int
+	month int
+	day   int
+	name  string
+}
+
+func isKarkkipaiva() (bool, string) {
+
+	now := time.Now()
+	wkint := uint(now.Weekday())
+	canHas := bool(false)
+	allowedDays := []uint{5, 6}
+	message := string("")
+
+	dudes := []Dude{
+		{2009, 1, 17, "Poe"},
+		{2012, 7, 30, "Milo"},
+		{1979, 8, 9, "Henryk"},
+		{1980, 8, 17, "Hannah"},
+		{1090, 9, 29, "Mörkö"},
+	}
+
+	// Check for Friday and Saturday
+	for i := 0; i < len(allowedDays); i++ {
+		cur := allowedDays[i]
+		if wkint == cur {
+			canHas = true
+		}
+	}
+
+	// Bdays
+	for i := 0; i < len(dudes); i++ {
+		if isBirthday(dudes[i]) {
+			canHas = true
+			//years := int(now.Year() - dudes[i].year)
+			message += "Hyvää synttäriä " + dudes[i].name
+		}
+	}
+
+	return canHas, message
+
+}
+
+func isBirthday(dude Dude) bool {
+	day := int(time.Now().Day())
+	month := int(time.Now().Month())
+
+	if dude.day == day && dude.month == month {
+		return true
+	}
+	return false
+}
+
+func main() {
+	canHasCandy, message := isKarkkipaiva()
+
+	if canHasCandy {
+		fmt.Println("HYVÄÄ KARKKIPÄIVÄÄ!!!\n")
+		if len(message) > 0 {
+			fmt.Println(message)
+		}
+	} else {
+		fmt.Println("TÄNÄÄN SYÖDÄÄN PUUROA!!!")
+	}
+
+}
